626c78ed9b0fe2f148663425ed8014fcd353cab4
TrueDoctor <d-kobert@web.de>
2018-11-06 17:30:42 +0000

    Update lexer.py

--- transpeter/lexer.py
+++ transpeter/lexer.py
@@ -3,19 +3,44 @@ import sys
 
 
 class Lexer:
-    __slots__ = ("program", "pattern")
-
     def __init__(self, program):
         self.program = program
-        regex = ["(?P<sep>\(|\)|{|}|;|,|\s)", "(?P<op>\+|-|\*|/|%)", "(?P<int>[0-9]+)", "(?P<id>[a-zA-Z_][a-zA-Z0-9_]*)"]
-        self.pattern = re.compile("|".join(regex))
+        self.keywords = []
+        regex = [
+            r'(?P<sep>\(|\)|{|}|;|,|\s+)',
+            r'(?P<op>=|\+|-|\*|/|%)',
+            r'(?P<int>[0-9]+)',
+            r'(?P<id>[a-zA-Z_][a-zA-Z0-9_]*)'
+        ]
+        self.pattern = re.compile('|'.join(regex))
 
     def tokenize(self):
         tokens = []
         line = 0
-        while True:
-            m = self.pattern.match("self.program")
-            if not m:
-                sys.exit("Error: invalid token in line {}".format(line))
-
+        self.program = re.sub(r'#.*', '', self.program)
+        while self.program:
+            match = self.pattern.match(self.program)
+            if not match:
+                sys.exit('Error: invalid token in line {}'.format(line))
+            line += self.program[:match.end()].count('\n')
+            self.program = self.program[match.end():]
+            for k, v in match.groupdict().items():
+                if v:
+                    if k == 'int':
+                        v = int(v)
+                    elif k == 'sep':
+                        v = re.sub(r'\s+', ' ', v)
+                    elif k == 'id' and v in self.keywords:
+                        k = 'key'
+                    tokens.append(Token(k, v, line))
         return tokens
+
+
+class Token:
+    def __init__(self, type, value, line):
+        self.type = type
+        self.value = value
+        self.line = line
+
+    def __repr__(self):
+        return '{}: {}'.format(self.type, self.value)
