423c776c1e6c0659eb3a22b3ff753a22bb0df4d3
TrueDoctor <d-kobert@web.de>
2018-11-06 17:28:11 +0000

    Update main.py

--- transpeter/main.py
+++ transpeter/main.py
@@ -1,4 +1,12 @@
 import sys
+import lexer
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) < 2:
+        exit('Error: no input file specified')
+    with open(sys.argv[1], 'r') as file:
+        lex = lexer.Lexer(file.read())
+    tokens = lex.tokenize()
+    for token in tokens:
+        size = len(str(token))
+        print(str(token) + " " * (20 - size) + 'in line ' + str(token.line))
