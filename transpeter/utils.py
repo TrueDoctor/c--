5ad8fe332b8a8ff56e012bac4ac8861a332ea2b5
konsumlamm <44230978+konsumlamm@users.noreply.github.com>
2018-11-19 10:54:39 +0100

    Update utils.py

--- transpeter/utils.py
+++ transpeter/utils.py
@@ -1,5 +1,5 @@
 class Token:
-     __slots__ = ('type', 'value', 'line')
+     __slots__ = ['type', 'value', 'line']
 
     def __init__(self, token_type, value, line):
         self.type = token_type
@@ -14,3 +14,46 @@ class Token:
             return self.type == other.type
         else:
             return self.type == other
+     
+     
+class AstNode:
+    def __init__(self, name, *nodes):
+        self.name = name
+        self.nodes = nodes
+
+    def print(self, level=0):
+        print(self.name)
+        level += 1
+        for node in self.nodes:
+            print('\t' * level, end='')
+            node.print(level)
+
+
+class Peekable:
+    __slots__ = ['gen', 'peek', 'eof']
+
+    def __init__(self, generator, eof=None):
+        self.gen = generator
+        self.peek = next(generator, eof)
+        self.eof = eof
+
+    def next(self):
+        temp = self.peek
+        self.peek = next(self.gen, self.eof)
+        return temp
+     
+     
+def print_tree(tree, prefix=""):  # doesn't work
+    if isinstance(tree, AstNode):
+        print(tree.name)
+        size = len(tree.nodes)
+        for i in range(size):
+            print(prefix, end="")
+            if i == size - 1:
+                print("\u2514\u2500", end="")
+                print_tree(tree.nodes[i], prefix + "  ")
+            else:
+                print("\u251c\u2500", end="")
+                print_tree(tree.nodes[i], prefix + "\u2502 ")
+    else:
+        print(tree)
