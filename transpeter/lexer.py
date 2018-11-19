47a471d259d589cde913c639480fd72af7ecb58a
konsumlamm <44230978+konsumlamm@users.noreply.github.com>
2018-11-19 10:59:16 +0100

    Update lexer.py

--- transpeter/lexer.py
+++ transpeter/lexer.py
@@ -8,28 +8,28 @@ class LexerError(Exception):
 
 
 class Lexer:
-    types = ['int']
-    control = ['if', 'else', 'while', 'for', 'return', 'inline']
+    ops = ['+', '-', '*', '/', '%', '==', '!=', '>=', '<=', '>', '<', 'or', 'and', 'not']
     separators = ['{', '}', '(', ')', ';', ',', '=']  # currently '=' is a separator
-    ops = ['+', '-', '*', '/', '%', '==', '!=', '>', '>=', '<', '<=', 'or', 'and', 'not']
-    keywords = ['void', *types, *control]
+    types = ['void', 'int']
+    control = ['if', 'else', 'while', 'for', 'return', 'inline']
 
-    def __init__(self, program):
-        self.program = re.sub(r'#.*', '', program)
+    def __init__(self):
         regex = [
             r'(?P<op>{})'.format('|'.join(re.escape(i) for i in Lexer.ops)),
             r'(?P<sep>{})'.format('|'.join(re.escape(i) for i in Lexer.separators)),
             r'(?P<int>[0-9]+|true|false)',
+            r'\'(?P<char>.|\n)\'',
             r'(?P<id>[a-zA-Z_][a-zA-Z0-9_]*)'
         ]
-        self.pattern = re.compile(r'\s*(?:{})\s*'.format('|'.join(regex)))
+        self.pattern = re.compile(r'\s*(?:{})'.format('|'.join(regex)))
 
-    def tokenize(self):
+    def tokenize(self, program):
+        program = re.sub(r'#.*', '', program.rstrip())
         pos = 0
         line = 1
-        size = len(self.program)
+        size = len(program)
         while pos < size:
-            match = self.pattern.match(self.program, pos)
+            match = self.pattern.match(program, pos)
             if not match:
                 raise LexerError('Error: invalid token in line {}'.format(line))
             pos = match.end()
@@ -43,9 +43,14 @@ class Lexer:
                             v = 0
                         else:
                             v = int(v)
+                    elif k == 'char':
+                        k = 'int'
+                        v = ord(v)
+                    elif k == 'id' and v in Lexer.types:
+                        k = 'type'
                     # if the token is an operator, a separator or a keyword
                     # k is set to v for easier parsing
-                    elif k == 'id' and v in Lexer.keywords:
+                    elif k == 'id' and v in Lexer.control:
                         k = v
                     elif k == 'op' or k == 'sep':
                         k = v
