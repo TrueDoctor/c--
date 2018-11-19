2ed0e6f9c3a379ff135c77e7afc8b33960bc92d0
konsumlamm <44230978+konsumlamm@users.noreply.github.com>
2018-11-19 11:03:37 +0100

    Update main.py

--- transpeter/main.py
+++ transpeter/main.py
@@ -1,12 +1,18 @@
-import sys
-import lexer
+from re import sub
+from sys import argv, exit
+
+from lexer import Lexer
+from parser import Parser
+from utils import print_tree
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(argv) < 2:
         exit('Error: no input file specified')
-    with open(sys.argv[1], 'r') as file:
-        lex = lexer.Lexer(file.read())
-    tokens = lex.tokenize()
-    for token in tokens:
-        size = len(str(token))
-        print(str(token) + " " * (20 - size) + 'in line ' + str(token.line))
+    with open(argv[1], 'r') as file:
+        code = file.read()
+    name = sub(r'.cmm$', '', argv[1])
+    lex = Lexer()
+    tokens = lex.tokenize(code)
+    parser = Parser(tokens)
+    ast = parser.parse(name)
+    print_tree(ast)
