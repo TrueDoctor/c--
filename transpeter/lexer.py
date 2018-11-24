84b18e2c4010ece75f07f4a8c4ea41916d7d7691
konsumlamm <44230978+konsumlamm@users.noreply.github.com>
2018-11-24 20:41:06 +0100

    added compound assignments

--- transpeter/lexer.py
+++ transpeter/lexer.py
@@ -8,15 +8,15 @@ class LexerError(Exception):
 
 
 class Lexer:
+    separators = ['=', '+=', '-=', '*=', '/=', '%=', '{', '}', '(', ')', ';', ',']  # currently '=' etc is a separator
     ops = ['+', '-', '*', '/', '%', '==', '!=', '>=', '<=', '>', '<', 'or', 'and', 'not']
-    separators = ['{', '}', '(', ')', ';', ',', '=']  # currently '=' is a separator
     types = ['void', 'int']
     control = ['if', 'else', 'while', 'for', 'return', 'inline']
 
     def __init__(self):
         regex = [
-            r'(?P<op>{})'.format('|'.join(re.escape(i) for i in Lexer.ops)),
             r'(?P<sep>{})'.format('|'.join(re.escape(i) for i in Lexer.separators)),
+            r'(?P<op>{})'.format('|'.join(re.escape(i) for i in Lexer.ops)),
             r'(?P<int>[0-9]+|true|false)',
             r'\'(?P<char>.|\s)\'',
             r'(?P<id>[a-zA-Z_][a-zA-Z0-9_]*)'
