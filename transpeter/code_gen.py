d6652e3064f2ff58c44f7f21711acc6ae579a461
konsumlamm <44230978+konsumlamm@users.noreply.github.com>
2018-11-27 21:00:30 +0100

    WIP

--- transpeter/code_gen.py
+++ transpeter/code_gen.py
@@ -6,9 +6,12 @@ class CodeGenerator:
     def __init__(self, ast):
         self.ast = ast
         self.funcs = []
+        for node in ast.nodes:  # maybe change
+            if node.name == 'func':
+                self.funcs.append(node[1])
         self.var_map = [{}]
         self.stack_ptr = 0
-        base_ptr = [0]
+        self.base_ptr = [0]
 
     def generate(self):
         code = '[{}]\n'.format(self.ast.name)  # header comment
@@ -24,8 +27,15 @@ class CodeGenerator:
     def _leave_scope(self):
         pass
 
-    def _declare_var(self):
+    def _declare_var(self, var):
         pass
+
+    def _get_var(self, var):  # pseudocode
+        for scope in self.var_map:
+            if var in scope:
+                return scope[var]
+        else:
+            raise CodeGenError('line {}: variable \'{}\' not declared'.format(var.line, var))
 
     def _eval_expr(self):
         pass
