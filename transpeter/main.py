d39c5a30a2f3ad3ca647df0ff3cd0fcdf856748e
NatrixAeria <jan.zwerschke@gmail.com>
2018-12-10 22:00:58 +0100

    Update main.py

--- transpeter/main.py
+++ transpeter/main.py
@@ -1,16 +1,16 @@
-from re import sub
-from sys import argv, exit
+import re
+import sys
 
 from lexer import Lexer
 from parser import Parser
 from utils import print_tree
 
 if __name__ == "__main__":
-    if len(argv) < 2:
-        exit('Error: no input file specified')
-    with open(argv[1], 'r') as file:
-        code = file.read()
-    name = sub(r'.cmm$', '', argv[1])
+    if len(sys.argv) < 2:
+        sys.exit('Error: no input file specified')
+    with open(sys.argv[1], 'r') as cmm_file:
+        code = cmm_file.read()
+    name = re.sub(r'.cmm$', '', sys.argv[1])
     lex = Lexer()
     tokens = lex.tokenize(code)
     parser = Parser(tokens)
