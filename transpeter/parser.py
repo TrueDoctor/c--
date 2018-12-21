7530188e8740f1598c3cbc60c2dd8b27a14a84b6
konsumlamm <44230978+konsumlamm@users.noreply.github.com>
2018-12-21 20:41:51 +0100

    Update parser.py

--- transpeter/parser.py
+++ transpeter/parser.py
@@ -1,5 +1,5 @@
 from utils import Peekable
-from astnode import *
+import astnode as ast
 
 
 class ParserError(Exception):
@@ -9,7 +9,7 @@ class ParserError(Exception):
 class Parser:
     __slots__ = 'tokens'
 
-    control = ['if', 'else', 'while', 'for', 'return', 'inline']
+    control = ['if', 'else', 'while', 'repeat', 'return', 'inline']
     EOF = 'eof'
 
     def __init__(self, tokens):
@@ -27,104 +27,104 @@ class Parser:
         instr = []
         while self.tokens.peek != Parser.EOF:
             if self.tokens.peek == 'type':  # function or declaration
-                var_type, name = self._parse_decl()
+                var_type, name, line = self.parse_decl()
                 if self.tokens.peek == '=':  # declaration with initialization
                     self.tokens.next()
-                    expr = self._parse_expr()
+                    expr = self.parse_expr()
                     self.expect(';')
-                    instr.append(Decl(None, var_type, name, expr))
+                    instr.append(ast.Decl(line, var_type, name, expr))
                 elif self.tokens.peek == ';':  # declaration without initialization
                     self.tokens.next()
-                    instr.append(Decl(None, var_type, name))
+                    instr.append(ast.Decl(line, var_type, name))
                 else:  # function
-                    args, block = self._parse_func()
-                    instr.append(Func(None, var_type, name, args, block))
+                    args, block = self.parse_func()
+                    instr.append(ast.Func(line, var_type, name, args, block))
             else:  # statement
-                instr.append(self._parse_statement())
-        ast = Program(program, instr)
-        return ast
+                instr.append(self.parse_statement())
+        tree = ast.Program(program, instr)
+        return tree
 
-    def _parse_func(self):  # returns args, statement (temp?)
+    def parse_func(self):  # returns args, statement (temp?)
         self.expect('(')
         args = []
         if self.tokens.peek != ')':
             arg_type = self.expect('type')
             name = self.expect('id')
-            args.append(Decl(None, arg_type.value, name.value))
+            args.append(ast.Decl(arg_type.line, arg_type.value, name.value))
         while self.tokens.peek != ')':
             self.expect(',')
             arg_type = self.expect('type')
             name = self.expect('id')
-            args.append(Decl(None, arg_type.value, name.value))
+            args.append(ast.Decl(arg_type.line, arg_type.value, name.value))
         self.tokens.next()
-        block = self._parse_block()
+        block = self.parse_block()
         return args, block
 
-    def _parse_block(self):  # blocks
+    def parse_block(self):  # blocks
         line = self.expect('{').line
         block = []
         while self.tokens.peek != '}':
             if self.tokens.peek == 'type':  # declaration
-                var_type, name = self._parse_decl()
+                var_type, name, line = self.parse_decl()
                 if self.tokens.peek == '=':
                     self.tokens.next()
-                    expr = self._parse_expr()
+                    expr = self.parse_expr()
                     self.expect(';')
-                    block.append(Decl(None, var_type, name, expr))
+                    block.append(ast.Decl(line, var_type, name, expr))
                 else:
                     self.expect(';')
-                    block.append(Decl(None, var_type, name))
+                    block.append(ast.Decl(line, var_type, name))
             else:  # statement
-                block.append(self._parse_statement())
+                block.append(self.parse_statement())
         self.tokens.next()
-        return Block(line, block)
+        return ast.Block(line, block)
 
-    def _parse_decl(self):
-        var_type = self.expect('type').value
+    def parse_decl(self):
+        next_token = self.expect('type')
         name = self.expect('id').value
-        return var_type, name
+        return next_token.value, name, next_token.line
 
-    def _parse_statement(self):
+    def parse_statement(self):
         if self.tokens.peek == '{':  # block
-            return self._parse_block()
-        elif self.tokens.peek in ('if', 'for', 'while'):  # for, while, if-else
+            return self.parse_block()
+        elif self.tokens.peek in ('if', 'repeat', 'while'):  # for, while, if-else
             ctrl = self.tokens.next()
             line = ctrl.line
             self.expect('(')
-            expr = self._parse_expr()
+            expr = self.parse_expr()
             self.expect(')')
-            stmnt = self._parse_statement()
+            stmnt = self.parse_statement()
             if ctrl == 'if':
                 if self.tokens.peek == 'else':
                     self.tokens.next()
-                    stmnt2 = self._parse_statement()
-                    return If(line, expr, stmnt, stmnt2)
+                    stmnt2 = self.parse_statement()
+                    return ast.If(line, expr, stmnt, stmnt2)
                 else:
-                    return If(line, expr, stmnt)
+                    return ast.If(line, expr, stmnt)
             elif ctrl == 'while':
-                return While(line, expr, stmnt)
+                return ast.While(line, expr, stmnt)
             else:
-                return For(line, expr, stmnt)
+                return ast.Repeat(line, expr, stmnt)
         elif self.tokens.peek == 'return':  # return
             line = self.tokens.next().line
-            expr = self._parse_expr()
+            expr = self.parse_expr()
             self.expect(';')
-            return Return(line, expr)
+            return ast.Return(line, expr)
         elif self.tokens.peek == 'inline':  # inline
             inline = self.tokens.next()
-            return Inline(inline.line, inline.value)
+            return ast.Inline(inline.line, inline.value)
         elif self.tokens.peek == 'id':
             name = self.tokens.next()
             if self.tokens.peek == '(':  # function call
-                args = self._parse_func_call()
+                args = self.parse_func_call()
                 self.expect(';')
-                return FuncCall(name.line, name.value, args)
+                return ast.FuncCall(name.line, name.value, args)
             next_token = self.tokens.next()
             if next_token in ('=', '+=', '-=', '*=', '/=', '%='):  # assignment
                 assign_op = next_token.type
-                expr = self._parse_expr()
+                expr = self.parse_expr()
                 self.expect(';')
-                return Assign(name.line, assign_op, name.value, expr)
+                return ast.Assign(name.line, assign_op, name.value, expr)
             else:
                 raise ParserError('line {}: expected function call or assignment'.format(next_token.line))
         elif self.tokens.peek == Parser.EOF:
@@ -132,85 +132,91 @@ class Parser:
         else:
             raise ParserError('line {}: unexpected token: \'{}\''.format(self.tokens.peek.line, self.tokens.peek.value))
 
-    def _parse_expr(self):
-        expr = self._parse_and()
+    def parse_expr(self):
+        expr = self.parse_and()
         while self.tokens.peek == 'or':
             line = self.tokens.next().line
-            next_expr = self._parse_and()
-            expr = BinOp(line, 'or', expr, next_expr)
+            next_expr = self.parse_and()
+            expr = ast.BinOp(line, 'or', expr, next_expr)
         return expr
 
-    def _parse_and(self):
-        expr = self._parse_equality()
+    def parse_and(self):
+        expr = self.parse_not()
         while self.tokens.peek == 'and':
             line = self.tokens.next().line
-            next_expr = self._parse_equality()
-            expr = BinOp(line, 'and', expr, next_expr)
+            next_expr = self.parse_not()
+            expr = ast.BinOp(line, 'and', expr, next_expr)
         return expr
-    
-    def _parse_equality(self):
-        expr = self._parse_relational()
+
+    def parse_not(self):
+        if self.tokens.peek == "not":
+            line = self.tokens.next().line
+            return ast.UnOp(line, "not", self.parse_equality())
+        return self.parse_equality()
+
+    def parse_equality(self):
+        expr = self.parse_relational()
         while self.tokens.peek in ('==', '!='):
             op = self.tokens.next()
-            next_expr = self._parse_relational()
-            expr = BinOp(op.line, op.type, expr, next_expr)
+            next_expr = self.parse_relational()
+            expr = ast.BinOp(op.line, op.type, expr, next_expr)
         return expr
 
-    def _parse_relational(self):
-        expr = self._parse_additive()
+    def parse_relational(self):
+        expr = self.parse_additive()
         while self.tokens.peek in ('<', '>', '<=', '>='):
             op = self.tokens.next()
-            next_expr = self._parse_additive()
-            expr = BinOp(op.line, op.type, expr, next_expr)
+            next_expr = self.parse_additive()
+            expr = ast.BinOp(op.line, op.type, expr, next_expr)
         return expr
 
-    def _parse_additive(self):
-        expr = self._parse_term()
+    def parse_additive(self):
+        expr = self.parse_term()
         while self.tokens.peek in ('+', '-'):
             op = self.tokens.next()
-            next_expr = self._parse_term()
-            expr = BinOp(op.line, op.type, expr, next_expr)
+            next_expr = self.parse_term()
+            expr = ast.BinOp(op.line, op.type, expr, next_expr)
         return expr
 
-    def _parse_term(self):
-        expr = self._parse_factor()
+    def parse_term(self):
+        expr = self.parse_factor()
         while self.tokens.peek in ('*', '/', '%'):
             op = self.tokens.next()
-            next_expr = self._parse_factor()
-            expr = BinOp(op.line, op.type, expr, next_expr)
+            next_expr = self.parse_factor()
+            expr = ast.BinOp(op.line, op.type, expr, next_expr)
         return expr
 
-    def _parse_factor(self):
+    def parse_factor(self):
         if self.tokens.peek == 'id':  # function call or variable access
             next_token = self.tokens.next()
             if self.tokens.peek == '(':
-                args = self._parse_func_call()
-                return FuncCall(next_token.line, next_token.value, args)
-            return Var(next_token.line, next_token.value)
+                args = self.parse_func_call()
+                return ast.FuncCall(next_token.line, next_token.value, args)
+            return ast.Var(next_token.line, next_token.value)
         elif self.tokens.peek == '(':  # parenthesis
             self.tokens.next()
-            expr = self._parse_expr()
+            expr = self.parse_expr()
             self.expect(')')
             return expr
-        elif self.tokens.peek in ('not', '-', '+'):  # unary operator
+        elif self.tokens.peek in ('-', '+'):  # unary operator
             op = self.tokens.next()
-            expr = self._parse_factor()
-            return UnOp(op.line, op.type, expr)
+            expr = self.parse_factor()
+            return ast.UnOp(op.line, op.type, expr)
         elif self.tokens.peek == 'int':  # integer literal
             val = self.tokens.next()
-            return Int(val.line, val.value)
+            return ast.Int(val.line, val.value)
         elif self.tokens.peek == Parser.EOF:
             raise ParserError('unexpected EOF')
         else:
             raise ParserError('line {}: unexpected token: \'{}\''.format(self.tokens.peek.line, self.tokens.peek.value))
 
-    def _parse_func_call(self):  # returns no node, only the arguments
+    def parse_func_call(self):  # returns no node, only the arguments
         self.tokens.next()  # is '('
         args = []
         if self.tokens.peek != ')':
-            args.append(self._parse_expr())
+            args.append(self.parse_expr())
         while self.tokens.peek != ')':
             self.expect(',')
-            args.append(self._parse_expr())
+            args.append(self.parse_expr())
         self.tokens.next()
         return args
