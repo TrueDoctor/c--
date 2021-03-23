44900e071bb6c7013e4ebf6a7045d5b9baf2497a
konsumlamm <konsumlamm@gmail.com>
2021-03-23 19:43:20 +0100

    Use rayon for parallel output tests

--- transpeter-rs/tests/output.rs
+++ transpeter-rs/tests/output.rs
@@ -3,9 +3,10 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
+use rayon::prelude::*;
+
 const CARGO: &str = "cargo";
 const CARGO_ARGS: [&str; 3] = ["run", "--quiet", "--"];
-const TESTS_DIR: &str = "tests/output";
 const CMM_EXT: &str = "cmm";
 
 fn check_out(path: &mut PathBuf, stream: &str, output: &[u8], test_name: &str) {
@@ -23,9 +24,8 @@ fn check_out(path: &mut PathBuf, stream: &str, output: &[u8], test_name: &str) {
     }
 }
 
-#[test]
-fn output() {
-    read_dir(TESTS_DIR)
+fn run_tests(dir: &str) {
+    let files = read_dir(dir)
         .unwrap()
         .map(Result::unwrap)
         .filter_map(|entry| {
@@ -33,31 +33,37 @@ fn output() {
             path.extension().filter(|&ext| ext == CMM_EXT)?;
             Some(path)
         })
-        .for_each(|mut path| {
-            let file_stem = path.file_stem().unwrap().to_string_lossy().into_owned();
-            eprintln!("  {} ...", file_stem);
-            let mut child = Command::new(CARGO)
-                .args(&CARGO_ARGS)
-                .args(&[path.to_str().unwrap(), "--run"])
-                .stdin(Stdio::piped())
-                .stdout(Stdio::piped())
-                .stderr(Stdio::piped())
-                .spawn()
-                .expect("failed to spawn command");
+        .collect::<Vec<_>>();
+    files.into_par_iter().for_each(|mut path| {
+        let file_stem = path.file_stem().unwrap().to_string_lossy().into_owned();
+        eprintln!("  {} ...", file_stem);
+        let mut child = Command::new(CARGO)
+            .args(&CARGO_ARGS)
+            .args(&[path.to_str().unwrap(), "--run"])
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("failed to spawn command");
 
-            // write to stdin
-            path.set_extension("stdin");
-            if let Ok(input) = read(&path) {
-                child
-                    .stdin
-                    .as_mut()
-                    .and_then(|stdin| stdin.write_all(&input).ok())
-                    .expect("failed to write to stdin");
-            }
+        // write to stdin
+        path.set_extension("stdin");
+        if let Ok(input) = read(&path) {
+            child
+                .stdin
+                .as_mut()
+                .and_then(|stdin| stdin.write_all(&input).ok())
+                .expect("failed to write to stdin");
+        }
 
-            // check stdout and stderr
-            let output = child.wait_with_output().expect("failed to wait on child");
-            check_out(&mut path, "stdout", &output.stdout, &file_stem);
-            check_out(&mut path, "stderr", &output.stderr, &file_stem);
-        });
+        // check stdout and stderr
+        let output = child.wait_with_output().expect("failed to wait on child");
+        check_out(&mut path, "stdout", &output.stdout, &file_stem);
+        check_out(&mut path, "stderr", &output.stderr, &file_stem);
+    });
+}
+
+#[test]
+fn output() {
+    run_tests("tests/output/");
 }
